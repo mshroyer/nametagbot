2b4cf0822003eb5fe9428afedac9a8ff6c8823c3
Mark Shroyer <mark@shroyer.name>
2018-06-23 20:11:14 -0700

    Use configparser for bot token

--- nametagbot/__init__.py
+++ nametagbot/__init__.py
@@ -1,3 +1,32 @@
+import appdirs
+import configparser
+import os
+
 from collections import namedtuple
 
 User = namedtuple('User', ['user_id', 'nick', 'avatar'])
+
+
+class Config:
+    def __init__(self, path=None):
+        if not path:
+            path = Config._default_path()
+
+        self.c = configparser.ConfigParser()
+        self.c.read(path)
+
+    def bot_token(self):
+        try:
+            return self.c['authentication']['BotToken']
+        except KeyError:
+            raise ConfigError('BotToken not configured')
+
+    @staticmethod
+    def _default_path():
+        return os.path.join(
+            appdirs.user_config_dir('nametagbot'), 'config.ini')
+
+
+class ConfigError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
