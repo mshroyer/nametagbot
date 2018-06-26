d366f411dc1916a12769710f42d6d38c0246e301
Mark Shroyer <mshroyer@google.com>
2018-06-25 23:02:29 -0700

    Add Google copyright headers

--- tests/test_avatar_cache.py
+++ tests/test_avatar_cache.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import os.path
 import pytest
 import responses
@@ -48,9 +62,7 @@ def test_does_not_request_cached_avatars(cache, tmpdir):
 @responses.activate
 def test_not_found_avatar_raises_exception(cache, tmpdir):
     responses.add(
-        responses.GET,
-        AVATAR_CDN_PREFIX + '123/avatar1.png',
-        status=404)
+        responses.GET, AVATAR_CDN_PREFIX + '123/avatar1.png', status=404)
 
     user = User('123', 'foo', 'avatar1')
     output_path = os.path.join(tmpdir, 'out.png')
